c64e502dfb794f3e0f10b5f8ad7cffa8b2ed1b66
hermilocap <hermilocap@hotmail.com>
2018-08-07 22:06:15 -0500

    Servicio web WCF en C#

---
+++ App_Code/IService.cs
@@ -0,0 +1,336 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+
+// NOTA: puede usar el comando "Rename" del menú "Refactorizar" para cambiar el nombre de interfaz "IService1" en el código y en el archivo de configuración a la vez.
+[ServiceContract]
+public interface IService
+{
+
+    [OperationContract]
+    void RegistraProducto(Productos producto);
+
+    [OperationContract]
+    void RegistrarVentas(Ventas ventas);
+
+    [OperationContract]
+    void RegistrarProveedor(Proveedores proveedor);
+
+    [OperationContract]
+    void RegistrarCompras(Compras compras);
+
+    [OperationContract]
+    Productos ConsultaIndividualProducto(string IdProducto);
+    
+    //nuevo
+    [OperationContract]
+    Ventas ConsultaIndividualVentas(string IdVenta);
+    
+    //nuevo
+    [OperationContract]
+    Proveedores ConsultaIndividualProveedores(string Idproveedor);
+    
+    //nuevo
+    [OperationContract]
+    Compras ConsultaIndividualCompras(string IdCompra);
+
+    [OperationContract]
+    List<Productos> ConsultaGeneralProductos();
+    
+    [OperationContract]
+    void EliminarProductos(string IdProducto);
+
+    [OperationContract]
+    void EliminarVentas(string IdVenta);
+
+    [OperationContract]
+    void EliminarProveedor(string Idproveedor);
+
+    [OperationContract]
+    void EliminarCompras(string IdCompra);
+
+    [OperationContract]
+    void ModificarProductos(Productos producto);
+
+}
+
+// Utilice un contrato de datos, como se ilustra en el ejemplo siguiente, para agregar tipos compuestos a las operaciones de servicio.
+[DataContract]
+public class Productos
+{
+    [DataMember]
+    string IdProducto;
+
+    public string IdProducto1
+    {
+        get { return IdProducto; }
+        set { IdProducto = value; }
+    }
+
+    [DataMember]
+    string Nombre_Producto;
+
+    public string Nombre_Producto1
+    {
+        get { return Nombre_Producto; }
+        set { Nombre_Producto = value; }
+    }
+    [DataMember]
+    string Cantidad_Existente;
+
+    public string Cantidad_Existente1
+    {
+        get { return Cantidad_Existente; }
+        set { Cantidad_Existente = value; }
+    }
+
+    [DataMember]
+    string Precio_Unitario;
+
+    public string Precio_Unitario1
+    {
+        get { return Precio_Unitario; }
+        set { Precio_Unitario = value; }
+    }
+
+    [DataMember]
+    string Categoria_Producto;
+
+    public string Categoria_Producto1
+    {
+        get { return Categoria_Producto; }
+        set { Categoria_Producto = value; }
+    }
+
+    [DataMember]
+    string Marca_Producto;
+
+    public string Marca_Producto1
+    {
+        get { return Marca_Producto; }
+        set { Marca_Producto = value; }
+    }
+ 
+
+    [DataMember]
+    string Descripcion_Producto;
+
+    public string Descripcion_Producto1
+    {
+        get { return Descripcion_Producto; }
+        set { Descripcion_Producto = value; }
+    }
+
+    [DataMember]
+    string ImagenMerc;
+
+    public string ImagenMerc1
+    {
+        get { return ImagenMerc; }
+        set { ImagenMerc = value; }
+    }
+}
+
+[DataContract]
+public class Proveedores
+{
+
+    [DataMember]
+string IdProveedor;
+
+public string IdProveedor1
+{
+  get { return IdProveedor; }
+  set { IdProveedor = value; }
+}
+
+    [DataMember]
+string NombreProveedor;
+
+public string NombreProveedor1
+{
+  get { return NombreProveedor; }
+  set { NombreProveedor = value; }
+}
+
+    [DataMember]
+string DireccionProveedor;
+
+public string DireccionProveedor1
+{
+  get { return DireccionProveedor; }
+  set { DireccionProveedor = value; }
+}
+
+    [DataMember]
+string TelefonoProveedor;
+
+public string TelefonoProveedor1
+{
+  get { return TelefonoProveedor; }
+  set { TelefonoProveedor = value; }
+}
+
+    [DataMember]
+string CiudadProveedor;
+
+public string CiudadProveedor1
+{
+  get { return CiudadProveedor; }
+  set { CiudadProveedor = value; }
+}
+
+    [DataMember]
+string EstadoProveedor;
+
+public string EstadoProveedor1
+{
+  get { return EstadoProveedor; }
+  set { EstadoProveedor = value; }
+}
+
+}
+
+[DataContract]
+public class Compras
+{
+    [DataMember]
+    string IdCompra;
+
+    public string IdCompra1
+    {
+        get { return IdCompra; }
+        set { IdCompra = value; }
+    }
+
+    [DataMember]
+    string IdProducto;
+
+    public string IdProducto1
+    {
+        get { return IdProducto; }
+        set { IdProducto = value; }
+    }
+
+    [DataMember]
+    string IdProveedor;
+
+    public string IdProveedor1
+    {
+      get { return IdProveedor; }
+      set { IdProveedor = value; }
+    }
+
+    [DataMember]
+    string NombreProveedor;
+
+    public string NombreProveedor1
+    {
+      get { return NombreProveedor; }
+      set { NombreProveedor = value; }
+    }
+
+    [DataMember]
+    string FechaDeCompra;
+
+    public string FechaDeCompra1
+    {
+        get { return FechaDeCompra; }
+        set { FechaDeCompra = value; }
+    }
+
+    [DataMember]
+    string CantidadComprada;
+
+    public string CantidadComprada1
+    {
+        get { return CantidadComprada; }
+        set { CantidadComprada = value; }
+    }
+
+    [DataMember]
+    string Total_Compra;
+
+    public string Total_Compra1
+    {
+        get { return Total_Compra; }
+        set { Total_Compra = value; }
+    } 
+}
+
+[DataContract]
+public class Ventas
+{
+    [DataMember]
+    string Id_Ventas;
+
+    public string Id_Ventas1
+    {
+        get { return Id_Ventas; }
+        set { Id_Ventas = value; }
+    }
+
+    [DataMember]
+    string IdProducto;
+
+    public string IdProducto1
+    {
+        get { return IdProducto; }
+        set { IdProducto = value; }
+    }
+
+    [DataMember]
+    string Cantidad_Venta;
+
+    public string Cantidad_Venta1
+    {
+        get { return Cantidad_Venta; }
+        set { Cantidad_Venta = value; }
+    }
+
+    [DataMember]
+    string Nombre_Producto;
+
+    public string Nombre_Producto1
+    {
+        get { return Nombre_Producto; }
+        set { Nombre_Producto = value; }
+    }
+
+    [DataMember]
+    string FechaVenta;
+
+    public string FechaVenta1
+    {
+        get { return FechaVenta; }
+        set { FechaVenta = value; }
+    }
+
+    [DataMember]
+    string Descuento;
+
+    public string Descuento1
+    {
+        get { return Descuento; }
+        set { Descuento = value; }
+    }
+
+    [DataMember]
+    string Total_Venta;
+
+    public string Total_Venta1
+    {
+        get { return Total_Venta; }
+        set { Total_Venta = value; }
+    }
+}
+
+
+
+
+	
+
---
+++ App_Code/Service.cs
@@ -0,0 +1,324 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+using DBTecnoScienceModel;
+
+
+// NOTA: puede usar el comando "Rename" del menú "Refactorizar" para cambiar el nombre de clase "Service1" en el código, en svc y en el archivo de configuración.
+public class Service : IService
+{
+
+    public void RegistraProducto(Productos producto)
+    {
+        try
+        {
+
+            using (DBTecnoScienceEntities2 entidad = new  DBTecnoScienceEntities2())
+            {
+                entidad.REGISTRARPRODUCTO(producto.IdProducto1, producto.Nombre_Producto1, producto.Cantidad_Existente1, producto.Precio_Unitario1,
+                    producto.Categoria_Producto1, producto.Marca_Producto1,producto.Descripcion_Producto1,producto.ImagenMerc1);
+                entidad.SaveChanges();
+            }
+        }
+        catch (Exception)
+        {
+
+        }
+
+    }
+
+    public void RegistrarVentas(Ventas ventas)
+    {
+        try
+        {
+
+            using (DBTecnoScienceEntities2 entidad = new DBTecnoScienceEntities2())
+            {
+                entidad.REGISTRARVENTAS(ventas.Id_Ventas1, ventas.IdProducto1,ventas.Cantidad_Venta1,
+                    ventas.Nombre_Producto1,ventas.FechaVenta1,ventas.Descuento1,ventas.Total_Venta1);
+                entidad.SaveChanges();
+            }
+        }
+        catch (Exception)
+        {
+
+        }
+    }
+
+    public void RegistrarProveedor(Proveedores proveedor)
+    {
+        try
+        {
+
+            using (DBTecnoScienceEntities2 entidad = new DBTecnoScienceEntities2())
+            {
+                entidad.REGISTRARPROVEEDORES(proveedor.IdProveedor1, proveedor.NombreProveedor1, proveedor.DireccionProveedor1, proveedor.TelefonoProveedor1, proveedor.CiudadProveedor1, proveedor.EstadoProveedor1);
+                entidad.SaveChanges();
+            }
+        }
+        catch (Exception)
+        {
+
+        }
+    }
+
+    public void RegistrarCompras(Compras compras)
+    {
+        try
+        {
+
+            using (DBTecnoScienceEntities2 entidad = new DBTecnoScienceEntities2())
+            {
+                entidad.REGISTRARCOMPRAS(compras.IdCompra1,compras.IdProducto1,compras.IdProveedor1,compras.NombreProveedor1,compras.FechaDeCompra1,compras.CantidadComprada1,compras.Total_Compra1);
+                entidad.SaveChanges();
+            }
+        }
+        catch (Exception)
+        {
+
+        }
+
+    }
+
+    public Productos ConsultaIndividualProducto(string IdProducto)
+    {
+        Productos producto = null; //Guardará los datos que se enviaran al cliente
+
+        using (DBTecnoScienceEntities2 entidad = new DBTecnoScienceEntities2())
+        {
+            var productoEncontrado = entidad.BUSCARPRODUCTO(IdProducto);
+            if (productoEncontrado != null)
+            {
+                foreach (var item in productoEncontrado)
+                {
+                    producto = new Productos();
+                    producto.IdProducto1 = item.IdProducto;
+                    producto.Nombre_Producto1 = item.Nombre_Producto;
+                    producto.Cantidad_Existente1 = item.Cantidad_Existente;
+                    producto.Precio_Unitario1 = item.Precio_Unitario;
+                    producto.Categoria_Producto1 = item.Categoria_Producto;
+                    producto.Marca_Producto1 = item.Marca_Producto;
+                    producto.Descripcion_Producto1 = item.Descripcion_Producto;
+                    producto.ImagenMerc1 = item.ImagenMerc;
+
+                }
+
+
+            }
+        }
+        return producto;
+    }
+
+
+    public  Ventas ConsultaIndividualVentas(string IdVenta)
+    {
+        Ventas venta = null; //Guardará los datos que se enviaran al cliente
+
+        using (DBTecnoScienceEntities2 entidad = new DBTecnoScienceEntities2())
+        {
+            var ventaEncontrado = entidad.BUSCARVENTA(IdVenta);
+            if (ventaEncontrado != null)
+            {
+                foreach (var item in ventaEncontrado)
+                {
+                    venta=new Ventas();
+                    venta.Id_Ventas1 = item.Id_Ventas;
+                    venta.IdProducto1 = item.IdProducto;
+                    venta.Cantidad_Venta1 = item.Cantidad_Venta;
+                    venta.Nombre_Producto1 = item.Nombre_Producto;
+                    venta.FechaVenta1 = item.FechaVenta;
+                    venta.Descuento1 = item.Descuento;
+                    venta.Total_Venta1 = item.Total_Venta;
+
+                }
+
+
+            }
+        }
+        return venta;
+    }
+
+    public Proveedores ConsultaIndividualProveedores(string Idproveedor)
+    {
+        Proveedores proveedores = null; //Guardará los datos que se enviaran al cliente
+
+        using (DBTecnoScienceEntities2 entidad = new DBTecnoScienceEntities2())
+        {
+            var provEncontrado = entidad.BUSCARPROVEEDORES(Idproveedor);
+            if (provEncontrado != null)
+            {
+                foreach (var item in provEncontrado)
+                {
+                    proveedores = new Proveedores();
+                    proveedores.IdProveedor1 = item.IdProveedor;
+                    proveedores.NombreProveedor1 = item.NombreProveedor;
+                    proveedores.DireccionProveedor1 = item.DireccionProveedor;
+                    proveedores.TelefonoProveedor1 = item.TelefonoProveedor;
+                    proveedores.CiudadProveedor1 = item.CiudadProveedor;
+                    proveedores.EstadoProveedor1 = item.EstadoProveedor;
+
+                }
+
+
+            }
+        }
+        return proveedores;
+    }
+
+    public Compras ConsultaIndividualCompras(string IdCompra)
+    {
+        Compras compra = null; //Guardará los datos que se enviaran al cliente
+
+        using (DBTecnoScienceEntities2 entidad = new DBTecnoScienceEntities2())
+        {
+            var compraEncontrado = entidad.BUSCARCOMPRAS(IdCompra);
+            if (compraEncontrado != null)
+            {
+                foreach (var item in compraEncontrado)
+                {
+                    compra = new Compras();
+                    compra.IdCompra1 = item.IdCompra;
+                    compra.IdProducto1 = item.IdProducto;
+                    compra.IdProveedor1 = item.IdProveedor;
+                    compra.NombreProveedor1 = item.Nombre_Proveedor;
+                    compra.FechaDeCompra1 = item.FechaDeCompra;
+                    compra.CantidadComprada1 = item.CantidadComprada;
+                    compra.Total_Compra1 = item.TotalCompra;
+
+                }
+            }
+        }
+        return compra;
+    }
+
+
+    public List<Productos> ConsultaGeneralProductos()
+    {
+
+        List<Productos> listaProductos = new List<Productos>();
+
+        Productos producto = null;
+
+        using (DBTecnoScienceEntities2 entidad = new DBTecnoScienceEntities2())
+        {
+            var productoEncontrado = entidad.CONSULTAGENERALPRODUCTO();
+
+            if (productoEncontrado != null)
+            {
+                foreach (var item in productoEncontrado)
+                {
+                    producto = new Productos();
+                    producto.IdProducto1 = item.IdProducto;
+                    producto.Nombre_Producto1 = item.Nombre_Producto;
+                    producto.Cantidad_Existente1 = item.Cantidad_Existente;
+                    producto.Precio_Unitario1 = item.Precio_Unitario;
+                    producto.Categoria_Producto1 = item.Categoria_Producto;
+                    producto.Marca_Producto1 = item.Marca_Producto;
+                    producto.Descripcion_Producto1 = item.Descripcion_Producto;
+                    producto.ImagenMerc1 = item.ImagenMerc;
+                    listaProductos.Add(producto);
+
+                }
+            }
+        }
+
+        return listaProductos;
+    }
+
+    public void EliminarProductos(string IdProducto)
+    {
+        try
+        {
+            using (DBTecnoScienceEntities2 entidad = new DBTecnoScienceEntities2())
+            {
+                entidad.ELIMINARPRODUCTO(IdProducto);
+                entidad.SaveChanges();
+            }
+        }
+        catch (Exception)
+        {
+
+        }
+
+    }
+
+    public void EliminarVentas(string IdVenta)
+    {
+        try
+        {
+            using (DBTecnoScienceEntities2 entidad = new DBTecnoScienceEntities2())
+            {
+                entidad.ELIMINARVENTAS( IdVenta);
+                entidad.SaveChanges();
+            }
+        }
+        catch (Exception)
+        {
+
+        }
+
+    }
+
+    public void EliminarProveedor(string IdProveedor)
+    {
+        try
+        {
+            using (DBTecnoScienceEntities2 entidad = new DBTecnoScienceEntities2())
+            {
+                entidad.ELIMINARPROVEEDORES(IdProveedor);
+                entidad.SaveChanges();
+            }
+        }
+        catch (Exception)
+        {
+
+        } 
+
+    }
+
+
+    public void EliminarCompras(string IdCompra)
+    {
+        try
+        {
+            using (DBTecnoScienceEntities2 entidad = new DBTecnoScienceEntities2())
+            {
+                entidad.ELIMINARCOMPRAS( IdCompra);
+                entidad.SaveChanges();
+            }
+        }
+        catch (Exception)
+        {
+
+        }
+
+    }
+
+    public void ModificarProductos(Productos producto)
+    {
+        try
+        {
+            using (DBTecnoScienceEntities2 entidad = new DBTecnoScienceEntities2())
+            {
+                entidad.MODIFICARPRODUCTO(producto.IdProducto1, producto.Nombre_Producto1, producto.Cantidad_Existente1, producto.Precio_Unitario1,
+                    producto.Categoria_Producto1, producto.Marca_Producto1, producto.Descripcion_Producto1, producto.ImagenMerc1);
+                entidad.SaveChanges();
+            }
+        }
+        catch (Exception)
+        {
+
+        }
+
+    }
+
+
+}
+
+
+
+
